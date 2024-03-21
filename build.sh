b9bd69cfc96eae189da736584cd332ed9b3736ec
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2024-03-21 09:22:57 -0600

    refactor: build with separate compilation

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ LIBS="`pkg-config --libs glew glfw3`"
 
 set -xe
 
-cc $CFLAGS -o triangle src/main.c src/filesystem.c src/algebra.c $LIBS
+mkdir -p build
+clang $CFLAGS -o build/main.o -c src/main.c
+clang $CFLAGS -o build/filesystem.o -c src/filesystem.c
+clang $CFLAGS -o build/algebra.o -c src/algebra.c
+
+clang -o triangle build/algebra.o build/filesystem.o build/main.o $LIBS
