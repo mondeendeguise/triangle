7d7c9aad6e287c51821103ea9b0ea4ac761fd847
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2024-03-21 09:24:12 -0600

    refactor: move vertices into renderer struct

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <string.h>
 
 #include "filesystem.h"
 #include "algebra.h"
@@ -65,7 +67,9 @@ bool load_shader_program(const char *vertex_shader_path,
                          GLuint *program)
 {
     bool ok = true;
-    GLuint vertex_shader, fragment_shader;
+    GLuint vertex_shader = 0;
+    GLuint fragment_shader = 0;
+
     ok = ok && compile_shader_file(vertex_shader_path, GL_VERTEX_SHADER, &vertex_shader);
     ok = ok && compile_shader_file(fragment_shader_path, GL_FRAGMENT_SHADER, &fragment_shader);
 
@@ -98,11 +102,12 @@ typedef enum {
     PROGRAM_COUNT,
 } Shader_Program;
 
+#define VERTEX_CAP (8 * 1024)
 typedef struct {
     GLuint vao;
     GLuint vbo;
     GLuint programs[PROGRAM_COUNT];
-    Vertex *vertices;
+    Vertex vertices[VERTEX_CAP];
     size_t vertex_count;
 } Renderer;
 
@@ -110,8 +115,6 @@ static Renderer global_renderer = {0};
 
 void r_init(Renderer *r)
 {
-    r->vertices = malloc(r->vertex_count * sizeof(Vertex));
-
     glGenBuffers(1, &r->vbo);
     glBindBuffer(GL_ARRAY_BUFFER, r->vbo);
     glBufferData(GL_ARRAY_BUFFER, r->vertex_count * sizeof(Vertex), r->vertices, GL_DYNAMIC_DRAW);
@@ -132,6 +135,12 @@ void r_reload_shaders(Renderer *r)
     glUseProgram(r->programs[0]);
 }
 
+void r_vertex(Renderer *r, Vertex v)
+{
+    assert(r->vertex_count < VERTEX_CAP);
+    r->vertices[r->vertex_count++] = v;
+}
+
 int main(void)
 {
     glfwInit();
@@ -139,7 +148,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "TRIANGLE", NULL, NULL);
     if(!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
@@ -152,35 +161,34 @@ int main(void)
     r_init(r);
     r_reload_shaders(r);
 
-    size_t vertex_count = 3;
-    Vertex *vertices = malloc(vertex_count * sizeof(Vertex));
+    /* size_t vertex_count = 3; */
+    /* Vertex *vertices = malloc(vertex_count * sizeof(Vertex)); */
     
-    vertices[0] = (Vertex) { v3f( 0.5f,  0.5f,  0.0f), v4f(1.0, 0.0, 0.0, 1.0) };
-    vertices[1] = (Vertex) { v3f( 0.5f, -0.5f,  0.0f), v4f(0.0, 1.0, 0.0, 1.0) };
-    vertices[2] = (Vertex) { v3f(-0.5f,  0.5f,  0.0f), v4f(0.0, 0.0, 1.0, 1.0) };
+    r_vertex(r, (Vertex) { v3f( 0.5f,  0.5f,  0.0f), v4f(1.0, 0.0, 0.0, 1.0) });
+    r_vertex(r, (Vertex) { v3f( 0.5f, -0.5f,  0.0f), v4f(0.0, 1.0, 0.0, 1.0) });
+    r_vertex(r, (Vertex) { v3f(-0.5f,  0.5f,  0.0f), v4f(0.0, 0.0, 1.0, 1.0) });
 
-    glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(Vertex), NULL, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, r->vertex_count * sizeof(Vertex), NULL, GL_DYNAMIC_DRAW);
 
-    vertex_count = 6;
-    vertices = realloc(vertices, vertex_count * sizeof(Vertex));
+    /* vertex_count = 6; */
+    /* vertices = realloc(r->vertices, r->vertex_count * sizeof(Vertex)); */
 
-    vertices[3] = (Vertex) { v3f( 0.5f, -0.5f,  0.0f), v4f(1.0f, 0.0f, 0.0f, 1.0f) };
-    vertices[4] = (Vertex) { v3f(-0.5f, -0.5f,  0.0f), v4f(0.0f, 1.0f, 0.0f, 1.0f) };
-    vertices[5] = (Vertex) { v3f(-0.5f,  0.5f,  0.0f), v4f(0.0f, 0.0f, 1.0f, 1.0f) };
+    r_vertex(r, (Vertex) { v3f(-0.5f, -0.5f,  0.0f), v4f(1.0f, 0.0f, 0.0f, 1.0f) });
+    r_vertex(r, (Vertex) { v3f( 0.5f, -0.5f,  0.0f), v4f(0.0f, 1.0f, 0.0f, 1.0f) });
+    r_vertex(r, (Vertex) { v3f(-0.5f,  0.5f,  0.0f), v4f(0.0f, 0.0f, 1.0f, 1.0f) });
 
-    glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(Vertex), vertices, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, r->vertex_count * sizeof(Vertex), r->vertices, GL_DYNAMIC_DRAW);
 
     while(!glfwWindowShouldClose(window)) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, vertex_count);
+        glDrawArrays(GL_TRIANGLES, 0, r->vertex_count);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
-    if(vertices) free(vertices);
     return 0;
 }
