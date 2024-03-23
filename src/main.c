9183b9201ac209fdad91a8d4c5b736cd6845e459
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2024-03-23 15:36:11 -0600

    chore: add error checks, logging, callbacks

--- src/main.c
+++ src/main.c
@@ -10,15 +10,28 @@
 
 #include "filesystem.h"
 #include "algebra.h"
+#include "logger.h"
+
+#define DEFAULT_WINDOW_WIDTH 800
+#define DEFAULT_WINDOW_HEIGHT 800
+
+#define return_defer(value) do { result = (value); goto defer; } while(0)
 
 const char *screen_shader_path = "shaders/screen.vert";
 const char *basic_fragment_shader_path = "shaders/basic.frag";
 const char *wireframe_shader_path = "shaders/wireframe.frag";
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+const char *shader_type_as_cstr(GLenum shader_type)
 {
-    (void) window;
-    glViewport(0, 0, width, height);
+    switch(shader_type) {
+        case GL_COMPUTE_SHADER:         return "GL_COMPUTE_SHADER";
+        case GL_VERTEX_SHADER:          return "GL_VERTEX_SHADER";
+        case GL_TESS_CONTROL_SHADER:    return "GL_TESS_CONTROL_SHADER";
+        case GL_TESS_EVALUATION_SHADER: return "GL_TESS_EVALUATION_SHADER";
+        case GL_GEOMETRY_SHADER:        return "GL_GEOMETRY_SHADER";
+        case GL_FRAGMENT_SHADER:        return "GL_FRAGMENT_SHADER";
+        default:                        return "(Unknown)";
+    }
 }
 
 typedef struct {
@@ -47,10 +60,10 @@ bool compile_shader_source(const GLchar *source, GLenum shader_type, GLuint *sha
         GLsizei message_size;
         glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &message_size);
         GLchar message[message_size];
-
         glGetShaderInfoLog(*shader, message_size, &message_size, message);
 
-        fprintf(stderr, "ERROR: failed to compile shader: %.*s\n", message_size, message);
+        LOG_ERROR("ERROR: failed to compile %s", shader_type_as_cstr(shader_type));
+        LOG_ERROR("%.*s", message_size, message);
         return false;
     }
 
@@ -60,28 +73,27 @@ bool compile_shader_source(const GLchar *source, GLenum shader_type, GLuint *sha
 bool compile_shader_file(const char *file_path, GLenum shader_type, GLuint *shader)
 {
     char *source = slurp_file(file_path);
+    if(source == NULL) {
+        LOG_ERROR("failed to read file `%s`: %s", file_path, strerror(errno));
+        return false;
+    }
+
     bool ok = compile_shader_source(source, shader_type, shader);
+    if(!ok) LOG_ERROR("failed to compile shader `%s`", file_path);
+
     free(source);
     return ok;
 }
 
-bool load_shader_program(const char *vertex_shader_path,
-                         const char *fragment_shader_path,
-                         GLuint *program)
+bool link_program(GLuint vertex_shader, GLuint fragment_shader, GLuint *program)
 {
-    bool ok = true;
-    GLuint vertex_shader = 0;
-    GLuint fragment_shader = 0;
-
-    ok = ok && compile_shader_file(vertex_shader_path, GL_VERTEX_SHADER, &vertex_shader);
-    ok = ok && compile_shader_file(fragment_shader_path, GL_FRAGMENT_SHADER, &fragment_shader);
-
     *program = glCreateProgram();
+
     glAttachShader(*program, vertex_shader);
     glAttachShader(*program, fragment_shader);
     glLinkProgram(*program);
 
-    int linked;
+    GLint linked = 0;
     glGetProgramiv(*program, GL_LINK_STATUS, &linked);
     if(!linked) {
         GLsizei message_size;
@@ -89,15 +101,28 @@ bool load_shader_program(const char *vertex_shader_path,
         GLchar message[message_size];
 
         glGetProgramInfoLog(*program, message_size, &message_size, message);
-
-        fprintf(stderr, "ERROR: failed to load shader program: %.*s\n", message_size, message);
+        LOG_ERROR("ERROR: failed to link shader program: %.*s\n", message_size, message);
     }
-    ok = ok && linked;
 
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
-    return ok;
+    return program;
+}
+
+bool load_shader_program(const char *vertex_shader_path,
+                         const char *fragment_shader_path,
+                         GLuint *program)
+{
+    GLuint vertex_shader = 0;
+    GLuint fragment_shader = 0;
+
+    if(!compile_shader_file(vertex_shader_path, GL_VERTEX_SHADER, &vertex_shader)) return false;
+    if(!compile_shader_file(fragment_shader_path, GL_FRAGMENT_SHADER, &fragment_shader)) return false;
+
+    if(!link_program(vertex_shader, fragment_shader, program)) return false;
+
+    return true;
 }
 
 typedef enum {
@@ -123,7 +148,13 @@ typedef struct {
 
 } Renderer;
 
+/* Global Variables */
+
 static Renderer global_renderer = {0};
+static double time = 0.0f;
+static bool pause = false;
+
+/* Renderer Functions */
 
 void r_init(Renderer *r)
 {
@@ -225,23 +256,145 @@ void r_quad_cr(Renderer *r, V2f center, V2f radius, V4f color)
     r_quad_pp(r, p1, p2, color);
 }
 
+/* Callbacks */
+
+static void glfw_error_callback(int error_code, const char *description)
+{
+    LOG_ERROR("%s (%d)", description, error_code);
+}
+
+static void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    (void) window;
+    glViewport(0, 0, width, height);
+}
+
+static void key_callback(GLFWwindow *window,
+                         int key, int scancode, int action, int mods)
+{
+    (void) scancode;
+    (void) mods;
+
+    if(action == GLFW_PRESS) {
+        switch(key) {
+            case GLFW_KEY_F5: {
+                r_reload_shaders(&global_renderer);
+            } break;
+
+            case GLFW_KEY_Q: {
+                glfwSetWindowShouldClose(window, GLFW_TRUE);
+            } break;
+
+            case GLFW_KEY_Z: {
+                toggle_wireframe(&global_renderer);
+            } break;
+        }
+    }
+}
+
+void gl_debug_message_callback(GLenum source,
+                               GLenum type,
+                               GLuint id,
+                               GLenum severity,
+                               GLsizei length,
+                               const GLchar *message,
+                               const void *data)
+{
+    (void) length;
+    (void) data;
+
+    char *_source;
+    char *_type;
+    char *_severity;
+
+    switch(source) {
+        case GL_DEBUG_SOURCE_API:             _source = "API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   _source = "WINDOW SYSTEM"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: _source = "SHADER COMPILER"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     _source = "THIRD PARTY"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:     _source = "APPLICATION"; break;
+        case GL_DEBUG_SOURCE_OTHER:
+        default:                              _source = "UNKNOWN"; break;
+    }
+
+    switch(type) {
+        case GL_DEBUG_TYPE_ERROR:               _type = "ERROR"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: _type = "DEPRECATED BEHAVIOR"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  _type = "UNDEFINED BEHAVIOR"; break;
+        case GL_DEBUG_TYPE_PORTABILITY:         _type = "PORTABILITY"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         _type = "PERFORMANCE"; break;
+        case GL_DEBUG_TYPE_OTHER:               _type = "OTHER"; break;
+        case GL_DEBUG_TYPE_MARKER:              _type = "MARKER"; break;
+        default:                                _type = "UNKNOWN"; break;
+    }
+
+    switch(severity) {
+        case GL_DEBUG_SEVERITY_HIGH:         _severity = "HIGH"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       _severity = "MEDIUM"; break;
+        case GL_DEBUG_SEVERITY_LOW:          _severity = "LOW"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: _severity = "NOTIFICATION"; break;
+        default:                             _severity = "UNKNOWN"; break;
+    }
+
+    fprintf(stderr, "[%s::%s/%s] (%d): %s", _source, _type, _severity, id, message);
+}
+
 int main(void)
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window;
+    int result = 0;
+    Renderer *r = &global_renderer;
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "TRIANGLE", NULL, NULL);
-    if(!window) {
-        glfwTerminate();
+    /* Initialize GLFW */
+
+    glfwSetErrorCallback(glfw_error_callback);
+    if(!glfwInit()) {
+        LOG_ERROR("failed to initialize GLFW");
         exit(EXIT_FAILURE);
     }
+
+    {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif // __APPLE__
+        
+        int v_major, v_minor, v_rev;
+        glfwGetVersion(&v_major, &v_minor, &v_rev);
+        LOG_INFO("GLFW %d.%d.%d", v_major, v_minor, v_rev);
+    }
+
+    window = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT,
+                              "TRIANGLE", NULL, NULL);
+    if(!window) {
+        LOG_ERROR("failed to create window");
+        return_defer(-1);
+    }
     glfwMakeContextCurrent(window);
-    glewInit();
+
+    glfwSetKeyCallback(window, key_callback);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    Renderer *r = &global_renderer;
+    /* Initialize GLEW */
+
+    {
+        GLenum err = glewInit();
+        if(err != GLEW_OK) {
+            LOG_ERROR("failed to initialize GLEW: %s", glewGetErrorString(err));
+            return_defer(-2);
+        }
+        LOG_INFO("GLEW %s", glewGetString(GLEW_VERSION));
+    }
+
+    LOG_INFO("OpenGL %s", glGetString(GL_VERSION));
+
+    if(glDebugMessageCallback != NULL) {
+        glEnable(GL_DEBUG_OUTPUT);
+        glDebugMessageCallback(gl_debug_message_callback, 0);
+    }
+
     r_init(r);
     r_reload_shaders(r);
 
@@ -262,8 +415,9 @@ int main(void)
 
     /* glBufferData(GL_ARRAY_BUFFER, r->vertex_count * sizeof(Vertex), r->vertices, GL_DYNAMIC_DRAW); */
 
-    toggle_wireframe(r);
-
+    time = glfwGetTime();
+    double previous_time = 0.0f;
+    double delta_time = 0.0f;
     while(!glfwWindowShouldClose(window)) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -273,9 +427,16 @@ int main(void)
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        double current_time = glfwGetTime();
+        delta_time = current_time - previous_time;
+        if(!pause) time += delta_time;
+        previous_time = current_time;
     }
 
+defer:
     r_deallocate(r);
+    if(window) glfwDestroyWindow(window);
     glfwTerminate();
-    return 0;
+    return result;
 }
